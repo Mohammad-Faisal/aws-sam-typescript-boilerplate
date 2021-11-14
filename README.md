29b3aab969646024759abeb5bd7d6eb83573aa46
Mohammad Faisal <faisal@amagroup.io>
2021-11-14 19:47:01 +0600

    Added Readme file for instructions to run on dev mode

---
+++ README.md
@@ -0,0 +1,11 @@
+## Enable development mode with hot reloading
+
+```bash
+NODE_ENV=development npm run watch
+```
+
+## Start the API server locally
+
+```bash
+sam local start-api
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
-const path = require("path");
-const AwsSamPlugin = require("aws-sam-webpack-plugin");
+const path = require('path');
+const AwsSamPlugin = require('aws-sam-webpack-plugin');
 
 const awsSamPlugin = new AwsSamPlugin();
 
 module.exports = {
-  entry: () => awsSamPlugin.entry(),
-  output: {
-    filename: (chunkData) => awsSamPlugin.filename(chunkData),
-    libraryTarget: "commonjs2",
-    path: path.resolve("."),
-  },
-  devtool: "source-map",
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  target: "node",
-  mode: process.env.NODE_ENV || "production",
-  module: {
-    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
-  },
-  plugins: [awsSamPlugin],
+    entry: () => awsSamPlugin.entry(),
+    output: {
+        filename: (chunkData) => awsSamPlugin.filename(chunkData),
+        libraryTarget: 'commonjs2',
+        path: path.resolve('.')
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    target: 'node',
+    externals: process.env.NODE_ENV === 'development' ? [] : ['aws-sdk'],
+    mode: process.env.NODE_ENV || 'production',
+    module: {
+        rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }]
+    },
+    plugins: [awsSamPlugin]
 };
